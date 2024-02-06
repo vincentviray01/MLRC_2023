7776bd704ea97cffe2168fe0cbe2b1752c71b28f
MikeyHV <mvillo01@outlook.com>
2024-02-05 20:26:17 -0700

    Add data_reduction parameter (default to 20%) to TrainDataset

--- src/datasets.py
+++ src/datasets.py
@@ -6,16 +6,18 @@ from torch.utils.data import Dataset
 import numpy as np
 
 class TrainImageNetDataset(Dataset):
-    def __init__(self, data_path, data_labels_path, label_mapping_path, transform):
+    def __init__(self, data_path, data_labels_path, label_mapping_path, transform, data_reduction = 0.2):
 
         self.data_paths = []
         self.data_labels = {}
         
         for path, subdirs, files in os.walk(data_path):
+            temp_data = []
             for name in files:
                 full_path = os.path.join(path, name)
-                self.data_paths.append(full_path)
-
+                temp_data.append(full_path)
+            temp_data = temp_data[:int(len(temp_data) * (1 - data_reduction))]
+            self.data_paths.extend(temp_data)
 
         label_mapping = {}
         print(label_mapping_path)
@@ -25,7 +27,7 @@ class TrainImageNetDataset(Dataset):
                 mapping = mapping[0]
                 id = mapping.split()[0]
                 label_mapping[id] = index
-                
+
         labels = {}
 
         for path in self.data_paths:
